69e50029ad4c59330168f749aa9a7acb889a18a7
MattNer0 <matteo.balloco@gmail.com>
2020-11-23 12:57:00 +0100

    remap

--- ios/RNAssetDelivery.h
+++ ios/RNAssetDelivery.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 
 @interface RNAssetDelivery : NSObject <RCTBridgeModule>
 
--- ios/RNAssetDelivery.m
+++ ios/RNAssetDelivery.m
@@ -4,71 +4,86 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(getPackState: (NSString *)name 
+RCT_REMAP_METHOD(getPackState:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
-    NSSet *tags = [NSSet setWithObjects: name];
-    // Use the shorter initialization method as all resources are in the main bundle
-    NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-    [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
-                                                     ^(BOOL resourcesAvailable)
-        {
-            if (resourcesAvailable) {
-                resolve(YES);
-            } else {
-                resolve(NO);
+    @try {
+        NSSet *tags = [NSSet setWithObjects: name];
+        // Use the shorter initialization method as all resources are in the main bundle
+        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
+                                                        ^(BOOL resourcesAvailable)
+            {
+                if (resourcesAvailable) {
+                    resolve(YES);
+                } else {
+                    resolve(NO);
+                }
             }
-        }
-    ];
+        ];
+    }
+    @catch(NSException *exception) {
+        reject(@"error", @"Couldn't get pack state.", exception);
+    }
 }
 
-RCT_EXPORT_METHOD(getPackLocation: (NSString *)name 
+RCT_REMAP_METHOD(getPackLocation:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
     resolve(YES);
 }
 
-RCT_EXPORT_METHOD(getPackContent: (NSString *)name 
+RCT_REMAP_METHOD(getPackContent:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
-    NSSet *tags = [NSSet setWithObjects: name];
-    // Use the shorter initialization method as all resources are in the main bundle
-    NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-    [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
-                                                     ^(BOOL resourcesAvailable)
-        {
-            if (resourcesAvailable) {
-                NSArray *filePaths = [NSBundle pathsForResourcesOfType:nil inDirectory:[[NSBundle resourceRequest.bundle] bundlePath]];
-                resolve(filePaths);
-            } else {
-                reject(@"error", @"resources not available", error);
+    @try {
+        NSSet *tags = [NSSet setWithObjects: name];
+        // Use the shorter initialization method as all resources are in the main bundle
+        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
+                                                        ^(BOOL resourcesAvailable)
+            {
+                if (resourcesAvailable) {
+                    NSArray *filePaths = [NSBundle pathsForResourcesOfType:nil inDirectory:[[NSBundle resourceRequest.bundle] bundlePath]];
+                    resolve(filePaths);
+                } else {
+                    reject(@"error", @"resources not available", error);
+                }
             }
-        }
-    ];
+        ];
+    }
+    @catch(NSException *exception) {
+        reject(@"error", @"Couldn't get pack content.", exception);
+    }
 }
 
-RCT_EXPORT_METHOD(fetchPack: (NSString *)name 
+RCT_REMAP_METHOD(fetchPack:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
-    NSSet *tags = [NSSet setWithObjects: name];
-    // Use the shorter initialization method as all resources are in the main bundle
-    NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-    resourceRequest.loadingPriority = NSBundleResourceRequestLoadingPriorityUrgent;
-    [resourceRequest beginAccessingResourcesWithCompletionHandler:
-                                 ^(NSError * __nullable error)
-        {
-            if (error) {
-                reject(@"error", @"resources not available", error);
-                return;
+    @try {
+        NSSet *tags = [NSSet setWithObjects: name];
+        // Use the shorter initialization method as all resources are in the main bundle
+        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        resourceRequest.loadingPriority = NSBundleResourceRequestLoadingPriorityUrgent;
+        [resourceRequest beginAccessingResourcesWithCompletionHandler:
+                                    ^(NSError * __nullable error)
+            {
+                if (error) {
+                    reject(@"error", @"resources not available", error);
+                    return;
+                }
+        
+                // The associated resources are loaded
+                resolve(YES);
             }
-    
-            // The associated resources are loaded
-            resolve(YES);
-        }
-    ];
+        ];
+    }
+    @catch(NSException *exception) {
+        reject(@"error", @"Couldn't fetch pack.", exception);
+    }
 }
 
 @end
