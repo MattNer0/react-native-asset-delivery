222def6e863fdaf9431c1d950e1ffe5c8f134a3f
MattNer0 <matteo.balloco@gmail.com>
2020-11-23 11:22:41 +0100

    paths

--- ios/RNAssetDelivery/RNAssetDelivery.m
+++ ios/RNAssetDelivery/RNAssetDelivery.m
@@ -21,6 +21,28 @@ RCT_REMAP_METHOD(getPackState:(NSString *)name, getPackStateWithResolver:(RCTPro
     ];
 }
 
+RCT_REMAP_METHOD(getPackLocation:(NSString *)name, getPackLocationWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    resolve(YES);
+}
+
+RCT_REMAP_METHOD(getPackContent:(NSString *)name, getPackContentWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+
+    NSSet *tags = [NSSet setWithObjects: name];
+    // Use the shorter initialization method as all resources are in the main bundle
+    NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+    [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
+                                                     ^(BOOL resourcesAvailable)
+        {
+            if (resourcesAvailable) {
+                NSArray *filePaths = [NSBundle pathsForResourcesOfType:nil inDirectory:[[NSBundle resourceRequest.bundle] bundlePath]];
+                resolve(filePaths);
+            } else {
+                reject(@"error", @"resources not available", error);
+            }
+        }
+    ];
+}
+
 RCT_REMAP_METHOD(fetchPack:(NSString *)name, fetchPackWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
 
     NSSet *tags = [NSSet setWithObjects: name];
