e4c83ff2b2ce16e5e5baf51700c46f74f61f1f68
MattNer0 <matteo.balloco@gmail.com>
2020-11-23 15:11:06 +0100

    tags try

--- ios/RNAssetDelivery.m
+++ ios/RNAssetDelivery.m
@@ -1,5 +1,9 @@
 #import "RNAssetDelivery.h"
 
+@interface RNAssetDelivery ()
+  @property (nonatomic) NSBundleResourceRequest *resourceRequest;
+@end
+
 @implementation RNAssetDelivery
 
 - (dispatch_queue_t)methodQueue
@@ -8,75 +12,85 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_REMAP_METHOD(getPackState,
-    name:(NSString *)name 
+RCT_EXPORT_METHOD(getPackState:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
     @try {
         NSSet *tags = [NSSet setWithObjects: name];
         // Use the shorter initialization method as all resources are in the main bundle
-        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-        [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
+        self.resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        [self.resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
                                                         ^(BOOL resourcesAvailable)
             {
                 if (resourcesAvailable) {
-                    resolve(YES);
+                    resolve(@(YES));
                 } else {
-                    resolve(NO);
+                    resolve(@(NO));
                 }
             }
         ];
     }
     @catch(NSException *exception) {
-        reject(@"error", @"Couldn't get pack state.", exception);
+        NSError err = [NSError errorWithDomain:exception.name code:0 userInfo:@{
+            NSUnderlyingErrorKey: exception,
+            NSDebugDescriptionErrorKey: exception.userInfo ?: @{ },
+            NSLocalizedFailureReasonErrorKey: (exception.reason ?: @"???")
+        }];
+        reject(@"error", @"Couldn't get pack state.", err);
     }
 }
 
-RCT_REMAP_METHOD(getPackLocation,
-    name:(NSString *)name 
+RCT_EXPORT_METHOD(getPackLocation:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
     resolve(YES);
 }
 
-RCT_REMAP_METHOD(getPackContent,
-    name:(NSString *)name 
+RCT_EXPORT_METHOD(getPackContent:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
     @try {
+        __weak typeof(self) weakSelf = self;
         NSSet *tags = [NSSet setWithObjects: name];
         // Use the shorter initialization method as all resources are in the main bundle
-        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-        [resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
+        self.resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        [self.resourceRequest conditionallyBeginAccessingResourcesWithCompletionHandler:
                                                         ^(BOOL resourcesAvailable)
             {
+                __strong typeof(weakSelf) strongSelf = weakSelf;
                 if (resourcesAvailable) {
-                    NSArray *filePaths = [NSBundle pathsForResourcesOfType:nil inDirectory:[[NSBundle resourceRequest.bundle] bundlePath]];
+                    NSBundle *resourceBundle = strongSelf.resourceRequest.bundle;
+                    NSArray *filePaths = [NSBundle pathsForResourcesOfType:nil inDirectory:[resourceBundle resourcePath]];
                     resolve(filePaths);
                 } else {
-                    reject(@"error", @"resources not available", error);
+                    NSError *errMsg = [NSError errorWithDomain:@"getPackContent" code:0 userInfo:nil];
+                    reject(@"error", @"resources not available", errMsg);
                 }
             }
         ];
     }
     @catch(NSException *exception) {
-        reject(@"error", @"Couldn't get pack content.", exception);
+        NSError err = [NSError errorWithDomain:exception.name code:0 userInfo:@{
+            NSUnderlyingErrorKey: exception,
+            NSDebugDescriptionErrorKey: exception.userInfo ?: @{ },
+            NSLocalizedFailureReasonErrorKey: (exception.reason ?: @"???")
+        }];
+        reject(@"error", @"Couldn't get pack content.", err);
     }
 }
 
-RCT_REMAP_METHOD(fetchPack,
-    name:(NSString *)name 
+RCT_EXPORT_METHOD(fetchPack:(NSString *)name 
     resolver:(RCTPromiseResolveBlock)resolve 
     rejecter:(RCTPromiseRejectBlock)reject) {
 
     @try {
         NSSet *tags = [NSSet setWithObjects: name];
         // Use the shorter initialization method as all resources are in the main bundle
-        NSBundleResourceRequest resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
-        resourceRequest.loadingPriority = NSBundleResourceRequestLoadingPriorityUrgent;
-        [resourceRequest beginAccessingResourcesWithCompletionHandler:
+        self.resourceRequest = [[NSBundleResourceRequest alloc] initWithTags:tags];
+        self.resourceRequest.loadingPriority = NSBundleResourceRequestLoadingPriorityUrgent;
+        [self.resourceRequest beginAccessingResourcesWithCompletionHandler:
                                     ^(NSError * __nullable error)
             {
                 if (error) {
@@ -85,7 +99,7 @@ RCT_REMAP_METHOD(fetchPack,
                 }
         
                 // The associated resources are loaded
-                resolve(YES);
+                resolve(@(YES));
             }
         ];
     }
